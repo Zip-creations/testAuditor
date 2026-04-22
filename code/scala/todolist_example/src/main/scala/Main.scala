a9b98bbf501fdca4e7c37cd7d6a67c0c7f746740
Ruben Rosenmeyer <ruben.rosenmeyer@active-group.de>
2026-04-22 17:29:34 +0200

    first version of todolist

--- code/scala/todolist_example/src/main/scala/Main.scala
+++ code/scala/todolist_example/src/main/scala/Main.scala
@@ -1,5 +1,30 @@
+import scala.collection.mutable.Map
+
 @main def hello(): Unit =
   println("Hello world!")
   println(msg)
+  var testlist = TODOList("sample", Map.empty[Int, TODOItem])
+  testlist.addEntry(TODOItem("testItem1"))
+  print(testlist.items)
 
 def msg = "I was compiled by Scala 3. :)"
+
+class TODOList(
+  var author: String,
+  var items: Map[Int, TODOItem]
+) {
+  var id = 0
+  def newID = () =>
+    id += 1
+    id
+  def addEntry = (item: TODOItem) =>
+    items(newID()) = item
+}
+  // var items = Map.empty[Int, TODOItem]
+  // if !itemsInit.isEmpty
+  // then 
+  //   var item = itemsInit    
+
+case class TODOItem (
+  content: String
+)

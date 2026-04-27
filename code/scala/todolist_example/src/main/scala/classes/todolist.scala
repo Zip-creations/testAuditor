85830f36d3d9eb5a02694778eb8ff0ca6d2fdfb2
Ruben Rosenmeyer <ruben.rosenmeyer@active-group.de>
2026-04-27 14:56:38 +0200

    added copy functionality & tests to todolist

--- code/scala/todolist_example/src/main/scala/classes/todolist.scala
+++ code/scala/todolist_example/src/main/scala/classes/todolist.scala
@@ -19,22 +19,40 @@ class TODOList(
   def removeAllItems = () =>
     items.clear()
   
-  def removeDuplicateItems = () =>
-    var seen = Set.empty[String]
-    items = items.filter { case (id, item) =>
-      if (seen.contains(item.content)) {
-        false
-      } else {
-        seen += item.content
-        true
-      }
-    }
+  // def removeDuplicateItems = () =>
+  //   var seen = Set.empty[String]
+  //   items = items.filter { case (id, item) =>
+  //     if (seen.contains(item.content)) {
+  //       false
+  //     } else {
+  //       seen += item.content
+  //       true
+  //     }
+  //   }
 
   def getAuthor = () =>
     author
   
   def getItems = () =>
     items.values.toList
+  
+  def getSize = () =>
+    items.size
+  
+  def copy = () =>
+    TODOList(author, items.clone())
+  
+  override def equals(obj: Any): Boolean = 
+    obj match {
+      case other: TODOList =>
+        this.author == other.author && this.items == other.items
+      case _ => false
+    }
+}
+
+object TODOList {
+  def empty() = 
+    TODOList("", Map.empty[Int, TODOItem])
 }
 
 case class TODOItem (
--- code/scala/todolist_example/src/test/scala/MySuite.scala
+++ code/scala/todolist_example/src/test/scala/MySuite.scala
@@ -7,7 +7,7 @@ import scala.collection.mutable.Map
 class TestAddingItems {
   @Test
   def testTodo(): Unit = {
-    var testlist = TODOList("sample", Map.empty[Int, TODOItem])
+    var testlist = TODOList.empty()
     var a = testlist.addItem(TODOItem("testItem1"))
     assertEquals(testlist.items(1).content, "testItem1")
   }
@@ -16,9 +16,34 @@ class TestAddingItems {
 class TestRemovingItems {
   @Test
   def testTodo(): Unit = {
-    var testlist = TODOList("sample", Map.empty[Int, TODOItem])
+    var testlist = TODOList.empty()
     var a = testlist.addItem(TODOItem("testItem1"))
     testlist.removeItemByID(1)
     assertFalse(testlist.items.contains(1))
+  }
+}
+
+class TestCopy {
+  @Test
+  def testEquality(): Unit = {
+    var list1 = TODOList.empty()
+    var list2 = TODOList.empty()
+    var list3 = list1.copy()
+    assertEquals(list1, list2)
+    assertEquals(list1, list3)
+    assertEquals(list2, list3)
+  }
+
+  @Test
+  def testCopy(): Unit = {
+    var list1 = TODOList.empty()
+    var list2 = list1.copy()
+    assertEquals(list1, list2)
+    list2.addItem(TODOItem("testItem1"))
+    var list3 = list2.copy()
+    assertNotEquals(list1, list2)
+    list2.removeAllItems()
+    assertEquals(list1, list2)
+    assertNotEquals(list2, list3)
   }
 }

f8b2425358b3c0c9a324ec21466400ddb13311b3
Ruben Rosenmeyer <ruben.rosenmeyer@active-group.de>
2026-04-29 15:51:42 +0200

    tool can now read xml files from dir

--- code/tooling_go/pipeline_observer/main.go
+++ code/tooling_go/pipeline_observer/main.go
@@ -1,7 +1,27 @@
 package main
 
 import "fmt"
+import "os"
 
 func main() {
-    fmt.Println("Hello, World!")
+	path := "./examples"// TODO: ask path on first start of tool. NiceToHave: Make it cofigurable
+	content, err := os.ReadDir(path)
+	content = filterForXML(content)
+	if err != nil {
+		fmt.Println("Error reading files: ", err)
+		return
+	}
+	for _, entry := range content {
+		fmt.Println(entry.Name())
+	}
+}
+
+func filterForXML(files []os.DirEntry) []os.DirEntry {
+	var xmlFiles []os.DirEntry
+	for _, file := range files {
+		if file.Name()[len(file.Name())-4:] == ".xml" {
+			xmlFiles = append(xmlFiles, file)
+		}
+	}
+	return xmlFiles
 }

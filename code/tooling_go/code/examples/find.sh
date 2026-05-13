a7e068df60b2eff9a0d108576ac62e423d811712
Ruben Rosenmeyer <ruben.rosenmeyer@active-group.de>
2026-05-13 14:57:04 +0200

    added find example to tooling

---
+++ code/tooling_go/code/examples/find.sh
@@ -0,0 +1,8 @@
+# example output of find.sh, pytest style
+echo '<?xml version="1.0" encoding="utf-8"?>'
+echo '<testsuites>'
+echo '    <testcase qualifiedName="test/test_identity.py::test_copy"/>'
+echo '    <testcase qualifiedName="test/test_simple.py::test_add_item"/>'
+echo '    <testcase qualifiedName="test/test_simple.py::test_removing_items"/>'
+echo '    <testcase qualifiedName="test/test_simple.py::test_skipping"/>'
+echo '</testsuites>'

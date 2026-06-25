41f9c14b713298dd8f9bccc8949fb12befda0dd5
Ruben Rosenmeyer <ruben.rosenmeyer@gmail.com>
2026-06-25 13:07:15 +0200

    fixing export

--- flake.nix
+++ flake.nix
@@ -1,22 +1,35 @@
 {
-  description = "A very basic flake";
+  description = "testAuditor Go tool";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: 
-    let pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
-    in
-  {
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      packages.${system}.default = pkgs.buildGoModule {
+        pname = "testAuditor";
+        version = "0.1.0";
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+        src = ./.;
 
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      buildInputs = [
-        pkgs.go
-      ];
+        vendorHash = null;
+
+        subPackages = [ "./src" ];
+      };
+
+      apps.${system}.default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/testAuditor";
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.go
+        ];
+      };
     };
-  };
 }

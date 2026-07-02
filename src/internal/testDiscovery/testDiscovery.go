09f391a6c576b614a4cf0c9252c1e2f07542335f
Ruben Rosenmeyer <ruben.rosenmeyer@gmail.com>
2026-07-02 13:01:46 +0200

    removed everything config related

--- src/internal/testDiscovery/testDiscovery.go
+++ src/internal/testDiscovery/testDiscovery.go
@@ -1,17 +1,7 @@
 package testDiscovery
 
 import "fmt"
-import "os/exec"
 import "encoding/xml"
-import cfg "github.com/Zip-creations/optimize_CI_deterministic_builds/src/internal/config"
-
-func RunTestDiscovery(command cfg.Command) (DiscoveryTestsuite, error) {
-	out, err := exec.Command(command.Command, command.Args...).CombinedOutput()
-	if err != nil {
-		return DiscoveryTestsuite{}, fmt.Errorf("Error executing test discovery script: %w\n%s", err, out)
-	}
-	return XMLtoDiscoveryTestsuite(string(out))
-}
 
 func XMLtoDiscoveryTestsuite(data string) (DiscoveryTestsuite, error) {
 	var suite DiscoveryTestsuite

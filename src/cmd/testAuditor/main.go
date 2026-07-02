ac0bb36e8c22d1eb39c4af2f738354cf9c44b3dc
Ruben Rosenmeyer <ruben.rosenmeyer@gmail.com>
2026-07-02 21:30:12 +0200

    testAuditor now returns empty list if all test have been ran

--- src/cmd/testAuditor/main.go
+++ src/cmd/testAuditor/main.go
@@ -12,13 +12,13 @@ func main() {
 	stdInput, err := io.ReadAll(os.Stdin)
 	if err != nil {
 		// write on Stderr, since Stdout is the expected channel for the produced XML
-		fmt.Fprintln(os.Stderr, "failed to read stdin:", err)
+		fmt.Fprintln(os.Stderr, "Failed to read stdin:", err)
 		os.Exit(1)
 	}
 	// Parse stdin
 	parsedInput, err := input.ParseInput(stdInput)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "failed to parse input:", err)
+		fmt.Fprintln(os.Stderr, "Failed to parse input:", err)
 		os.Exit(1)
 	}
 	// fmt.Println("ParseInput result:", parsedInput)  // Debug
@@ -30,8 +30,9 @@ func main() {
 		os.Exit(1)
 	}
 
+	// 0 tests in test discovey means there are no tests expected in the project
 	if len(allSuites.DiscoveryTestcases) == 0 {
-		fmt.Fprintln(os.Stderr, "test discovery resulted in 0 tests found. Aborting.")
+		fmt.Fprintln(os.Stderr, "Test discovery resulted in 0 tests found. Aborting.")
 		os.Exit(0)
 	}
 	// fmt.Println("ParseTestDiscovery result:", allSuites)  // Debug
@@ -44,12 +45,7 @@ func main() {
 	}
 	// fmt.Println("ParseJUnitTestSuites result:", existingReports)  // Debug
 
-	report := out.MatchTests(allSuites, existingReports)
-	if len(report) == 0 {
-		// write on Stderr, since Stdout is the expected route for the produced XML
-		fmt.Fprintln(os.Stderr, "All discovered tests have already been executed.\nTerminating.")
-		os.Exit(0)
-	}
+	result := out.MatchTests(allSuites, existingReports)
 	// Write result to stdout
-	fmt.Println(report)
+	fmt.Println(result)
 }
